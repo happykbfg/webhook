c8756e1ad47a54a882151e00f8a65f2b1f1f41d5
happycloudpak <happycloudpak@gmail.com>
2021-02-02 20:11:29 +0900

    add log

--- src/main/java/com/springcloud/Controller.java
+++ src/main/java/com/springcloud/Controller.java
@@ -1,5 +1,7 @@
 package com.springcloud;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -11,12 +13,15 @@ import io.swagger.annotations.ApiOperation;
 @RestController
 @RefreshScope
 public class Controller {
+	private final Logger log = LoggerFactory.getLogger(getClass());
+	
     @Value("${greeting:Hi}")
     private String greeting;
    
     @GetMapping("/greeting/{message}")
     @ApiOperation(value="Test Spring cloud bus")
     public String echo(@PathVariable String message) {
+    	log.info("### webhook > /greeting/${message}");
         return greeting + " => " + message;
     } 
 }
